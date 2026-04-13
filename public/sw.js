77beddaded67706e57b7856a5f5b3e40e40d13f5
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 20:13:07 +0200

    Increase edit mode padding to 30 cells around layout

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v37';
+const CACHE_NAME = 'yard-manager-v38';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
