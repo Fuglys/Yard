a9866cb8fe9d4d74c20de04ed9766dba21e4673e
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 20:08:17 +0200

    Fix init flow: server first, no auto-sync on saveLayout, prevent data loss

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v36';
+const CACHE_NAME = 'yard-manager-v37';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
