ecb5e4c3bf187939c6a6ef477b5e9eebee098959
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 21:37:57 +0200

    Keep individual cells clickable behind merged regions

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v47';
+const CACHE_NAME = 'yard-manager-v48';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
