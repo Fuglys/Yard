8ed9033a21863de99b8a9f87a12d16dd1bc119de
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 21:25:44 +0200

    Merge all cell types: custom, zak material (vertical), balen material, with labels

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v44';
+const CACHE_NAME = 'yard-manager-v45';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
