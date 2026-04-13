1ec26f0dcc1e94414204d201b1488010aa9ea7c1
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 21:06:01 +0200

    Shift layout 25 cols right, expand grid to 125x100

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v43';
+const CACHE_NAME = 'yard-manager-v44';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
