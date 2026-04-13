b695a00ef5cb8fd4c9ba2e7f5864c4a81825ae95
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 22:19:29 +0200

    Same fill color for all balen, thick dark border per material group

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v55';
+const CACHE_NAME = 'yard-manager-v56';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
