4e53b82f53c515cb60c6c320b27decb8ce041e0e
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 20:32:02 +0200

    Multi-select in edit mode, undo/redo, apply tool on release

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v40';
+const CACHE_NAME = 'yard-manager-v41';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
