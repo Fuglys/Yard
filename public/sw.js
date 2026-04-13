552de76280dd95fa99760a6eedc7f2479193e220
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 19:54:11 +0200

    Modern light theme, consistent custom type colors via meta, neutral palette

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v33';
+const CACHE_NAME = 'yard-manager-v34';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
