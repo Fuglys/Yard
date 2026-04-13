e9beaaac66a4fad9ae31bab3cdb54250abedf8f6
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 21:45:12 +0200

    Filter invalid sync queue entries, clean up stuck items

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v49';
+const CACHE_NAME = 'yard-manager-v50';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
