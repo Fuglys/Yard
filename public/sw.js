bd457ed4b4a8a2af93c17e1e9d0dab38e3e0b5d5
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 20:51:43 +0200

    Fix numbering: support descending (120 to 56), sorted placement

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v41';
+const CACHE_NAME = 'yard-manager-v42';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
