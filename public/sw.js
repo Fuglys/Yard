ebcad53498d20d110f533568a4a2fa6e8e412066
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 22:30:04 +0200

    Desaturated neutral color palette for all material types

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v57';
+const CACHE_NAME = 'yard-manager-v58';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
