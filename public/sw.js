71d87927a84270f72c82d478b2fc97ce277a552e
Yard Manager <yard-manager@cirrec.nl>
2026-04-13 19:58:52 +0200

    Merge adjacent cells: full merge for non-editable, horizontal-only for zak-num

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yard-manager-v34';
+const CACHE_NAME = 'yard-manager-v35';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/style.css'];
 
 self.addEventListener('install', (event) => {
